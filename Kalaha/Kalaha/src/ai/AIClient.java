d83d416a9bcb11e1c0ad47e3889219e9b4d86549
tdemulier <tdemulie@insa-rennes.fr>
2014-09-24 17:01:34 +0200

    V0.1

--- Kalaha/Kalaha/src/ai/AIClient.java
+++ Kalaha/Kalaha/src/ai/AIClient.java
@@ -5,143 +5,128 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 import java.awt.*;
+import static java.lang.System.currentTimeMillis;
+import java.util.ArrayList;
 import kalaha.*;
 
 /**
- * This is the main class for your Kalaha AI bot. Currently
- * it only makes a random, valid move each turn.
- * 
+ * This is the main class for your Kalaha AI bot. Currently it only makes a
+ * random, valid move each turn.
+ *
  * @author Johan Hagelbäck
  */
-public class AIClient implements Runnable
-{
+public class AIClient implements Runnable {
+
     private int player;
     private JTextArea text;
-    
+
     private PrintWriter out;
     private BufferedReader in;
     private Thread thr;
     private Socket socket;
     private boolean running;
     private boolean connected;
-    	
+
     /**
      * Creates a new client.
      */
-    public AIClient()
-    {
-	player = -1;
+    public AIClient() {
+        player = -1;
         connected = false;
-        
+
         //This is some necessary client stuff. You don't need
         //to change anything here.
         initGUI();
-	
-        try
-        {
+
+        try {
             addText("Connecting to localhost:" + KalahaMain.port);
             socket = new Socket("localhost", KalahaMain.port);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             addText("Done");
             connected = true;
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             addText("Unable to connect to server");
             return;
         }
     }
-    
+
     /**
      * Starts the client thread.
      */
-    public void start()
-    {
+    public void start() {
         //Don't change this
-        if (connected)
-        {
+        if (connected) {
             thr = new Thread(this);
             thr.start();
         }
     }
-    
+
     /**
      * Creates the GUI.
      */
-    private void initGUI()
-    {
+    private void initGUI() {
         //Client GUI stuff. You don't need to change this.
         JFrame frame = new JFrame("My AI Client");
         frame.setLocation(Global.getClientXpos(), 445);
-        frame.setSize(new Dimension(420,250));
+        frame.setSize(new Dimension(420, 250));
         frame.getContentPane().setLayout(new FlowLayout());
-        
+
         text = new JTextArea();
         JScrollPane pane = new JScrollPane(text);
         pane.setPreferredSize(new Dimension(400, 210));
-        
+
         frame.getContentPane().add(pane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         frame.setVisible(true);
     }
-    
+
     /**
      * Adds a text string to the GUI textarea.
-     * 
+     *
      * @param txt The text to add
      */
-    public void addText(String txt)
-    {
+    public void addText(String txt) {
         //Don't change this
         text.append(txt + "\n");
         text.setCaretPosition(text.getDocument().getLength());
     }
-    
+
     /**
-     * Thread for server communication. Checks when it is this
-     * client's turn to make a move.
+     * Thread for server communication. Checks when it is this client's turn to
+     * make a move.
      */
-    public void run()
-    {
+    public void run() {
         String reply;
         running = true;
-        
-        try
-        {
-            while (running)
-            {
+
+        try {
+            while (running) {
                 //Checks which player you are. No need to change this.
-                if (player == -1)
-                {
+                if (player == -1) {
                     out.println(Commands.HELLO);
                     reply = in.readLine();
 
                     String tokens[] = reply.split(" ");
                     player = Integer.parseInt(tokens[1]);
-                    
+
                     addText("I am player " + player);
                 }
-                
+
                 //Check if game has ended. No need to change this.
                 out.println(Commands.WINNER);
                 reply = in.readLine();
-                if(reply.equals("1") || reply.equals("2") )
-                {
+                if (reply.equals("1") || reply.equals("2")) {
                     int w = Integer.parseInt(reply);
-                    if (w == player)
-                    {
+                    if (w == player) {
                         addText("I won!");
-                    }
-                    else
-                    {
+                    } else {
                         addText("I lost...");
                     }
                     running = false;
                 }
-                if(reply.equals("0"))
-                {
+                if (reply.equals("0")) {
                     addText("Even game!");
                     running = false;
                 }
@@ -149,81 +134,145 @@ public class AIClient implements Runnable
                 //Check if it is my turn. If so, do a move
                 out.println(Commands.NEXT_PLAYER);
                 reply = in.readLine();
-                if (!reply.equals(Errors.GAME_NOT_FULL) && running)
-                {
+                if (!reply.equals(Errors.GAME_NOT_FULL) && running) {
                     int nextPlayer = Integer.parseInt(reply);
 
-                    if(nextPlayer == player)
-                    {
+                    if (nextPlayer == player) {
                         out.println(Commands.BOARD);
                         String currentBoardStr = in.readLine();
                         boolean validMove = false;
-                        while (!validMove)
-                        {
+                        while (!validMove) {
                             long startT = System.currentTimeMillis();
                             //This is the call to the function for making a move.
                             //You only need to change the contents in the getMove()
                             //function.
                             GameState currentBoard = new GameState(currentBoardStr);
                             int cMove = getMove(currentBoard);
-                            
+
                             //Timer stuff
                             long tot = System.currentTimeMillis() - startT;
-                            double e = (double)tot / (double)1000;
-                            
+                            double e = (double) tot / (double) 1000;
+
                             out.println(Commands.MOVE + " " + cMove + " " + player);
                             reply = in.readLine();
-                            if (!reply.startsWith("ERROR"))
-                            {
+                            if (!reply.startsWith("ERROR")) {
                                 validMove = true;
                                 addText("Made move " + cMove + " in " + e + " secs");
                             }
                         }
                     }
                 }
-                
+
                 //Wait
                 Thread.sleep(100);
             }
-	}
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             running = false;
         }
-        
-        try
-        {
+
+        try {
             socket.close();
             addText("Disconnected from server");
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             addText("Error closing connection: " + ex.getMessage());
         }
     }
-    
+
     /**
-     * This is the method that makes a move each time it is your turn.
-     * Here you need to change the call to the random method to your
-     * Minimax search.
-     * 
+     * This is the method that makes a move each time it is your turn. Here you
+     * need to change the call to the random method to your Minimax search.
+     *
      * @param currentBoard The current board state
      * @return Move to make (1-6)
      */
-    public int getMove(GameState currentBoard)
-    {
-        int myMove = getRandom();
+    public int getMove(GameState currentBoard) {
+        int myMove;
+        GameState clonedBoard = currentBoard.clone();
+        GameNode root;
+        int maxLevel = 2;
+        long start = currentTimeMillis();
+        int currentMax;
+        
+        do {
+            root = new GameNode(clonedBoard, 0);
+            currentMax = expandTree(root, maxLevel);
+            maxLevel += 2;
+        } while (currentTimeMillis() - start < 1000);
+
+        //int currentMax = expandTree(root, maxLevel);
+        myMove = root.children.get(0).move;
+        for (GameNode c : root.children) {
+            if (c.minMax == currentMax) {
+                myMove = c.move;
+            }
+        }
         return myMove;
     }
-    
+
+    public int expandTree(GameNode node, int maxLevel) {
+        int validMoves = node.state.getNoValidMoves(node.state.getNextPlayer());
+        if (node.level < maxLevel && validMoves > 0) {
+            int minMax = 0;
+            for (int move = 1; move <= 6; move++) {
+                if (node.state.moveIsPossible(move)) {
+                    GameState childState = node.state.clone();
+                    childState.makeMove(move);
+                    GameNode child = new GameNode(childState, node.level + 1, move);
+                    node.addChild(child);
+                    minMax = minMax(node.level, minMax, expandTree(child, maxLevel));
+                }
+            }
+            node.minMax = minMax;
+            return minMax;
+        } else if (validMoves == 0) {
+            return utility(node.state);
+        } else {
+            return evaluation(node.state);
+        }
+    }
+
+    public int minMax(int level, int currentMinMax, int value) {
+        return (level % 2 == 1) ? Math.max(currentMinMax, value) : Math.min(currentMinMax, value);
+    }
+
+    public int utility(GameState state) {
+        return (player == 1) ? state.getScore(1) - state.getScore(2) : state.getScore(2) - state.getScore(1);
+    }
+
+    public int evaluation(GameState state) {
+        return utility(state);
+    }
+
+    public class GameNode {
+
+        public int minMax;
+        public int move;
+        public int level;
+        public GameState state;
+        public ArrayList<GameNode> children = new ArrayList<>();
+
+        public GameNode(GameState state, int level) {
+            this.state = state;
+            this.level = level;
+        }
+
+        public GameNode(GameState state, int level, int move) {
+            this.state = state;
+            this.level = level;
+            this.move = move;
+        }
+
+        public void addChild(GameNode child) {
+            children.add(child);
+        }
+    }
+
     /**
-     * Returns a random ambo number (1-6) used when making
-     * a random move.
-     * 
+     * Returns a random ambo number (1-6) used when making a random move.
+     *
      * @return Random ambo number
      */
-    public int getRandom()
-    {
-        return 1 + (int)(Math.random() * 6);
+    public int getRandom() {
+        return 1 + (int) (Math.random() * 6);
     }
 }
